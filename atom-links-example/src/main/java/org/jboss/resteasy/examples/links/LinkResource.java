7d6f095a8d34a607cac129a4423747b8f1533b15
阿男 <l.weinan@gmail.com>
2020-02-27 19:00:46 +0800

    update

--- atom-links-example/src/main/java/org/jboss/resteasy/examples/links/LinkResource.java
+++ atom-links-example/src/main/java/org/jboss/resteasy/examples/links/LinkResource.java
@@ -1,4 +1,12 @@
 package org.jboss.resteasy.examples.links;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/link")
 public class LinkResource {
+    @GET
+    public String get() {
+        return "";
+    }
 }
